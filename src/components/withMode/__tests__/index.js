b74e0fbe9c2940aa1d1e56d207d5e7fce9a0d725
Brian Kang <qmfkdldks@gmail.com>
2020-10-12 13:02:47 -0300

    Add withMode hoc

---
+++ src/components/withMode/__tests__/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { mount } from "enzyme";
+import Sneak from "../../../animations/sneak";
+import withMode, { MODES } from "../index";
+import { useInView } from "react-intersection-observer";
+
+jest.mock("react-intersection-observer", () => ({
+  useInView: jest.fn(() => ({ ref: jest.fn(), inView: true })),
+}));
+
+jest.unmock("framer-motion");
+import * as framerMotion from "framer-motion";
+const mockStartFunction = jest.fn();
+framerMotion.useAnimation = jest
+  .fn()
+  .mockReturnValue({ start: mockStartFunction });
+
+const SneakWithMode = withMode(Sneak);
+
+describe("withMode", () => {
+  describe("when mode is RPEAT", () => {
+    it("transition loop is infinity", () => {
+      const wrapper = mount(
+        <SneakWithMode mode={MODES.REPEAT}>Hide and sneak</SneakWithMode>
+      );
+      expect(wrapper.find(Sneak).props()).toEqual({
+        control: "start",
+        transition: { loop: Infinity, repeatDelay: 3 },
+        children: "Hide and sneak",
+      });
+    });
+  });
+
+  describe("when mode is INVIEW", () => {
+    it("starts animation when inView is true", () => {
+      const wrapper = mount(
+        <SneakWithMode mode={MODES.INVIEW}>Hide and sneak</SneakWithMode>
+      );
+
+      expect(useInView).toBeCalled();
+      expect(wrapper.find(Sneak).props()).toEqual({
+        control: "start",
+        children: "Hide and sneak",
+      });
+    });
+  });
+
+  describe("when mode is HOVER", () => {
+    it("calls useAnimation", () => {
+      const wrapper = mount(
+        <SneakWithMode mode={MODES.HOVER}>Hide and sneak</SneakWithMode>
+      );
+
+      expect(framerMotion.useAnimation).toBeCalled();
+      wrapper.find("span").first().simulate("mouseenter");
+      expect(mockStartFunction).toBeCalledWith("start");
+    });
+  });
+});
