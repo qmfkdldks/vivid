ff9769789e13638ccbb272d3bc6442400595c933
juanmitriatti <juan-pablo.mitriatti@wwecorp.com>
2020-10-15 10:05:56 -0300

    Doing a rebase with master.

--- src/animations/sneak/index.js
+++ src/animations/sneak/index.js
@@ -1,4 +1,107 @@
 import Component from "./component";
 import withMode from "../../components/withMode";
-
 export default withMode(Component);
+import React from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+ 
+
+
+const jump = (seed = 0) => {
+  return [0, 0, -30, 0, -5, 0, 0];
+};
+
+const jumpWidth = (seeed) => [1.2, 0.8, 1.05, 1, 1, 1];
+const jumpHeight = (seeed) => [0.7, 1.2, 0.95, 1, 1, 1];
+
+const goLeft = (seed = 0) => {
+  return [
+    seed + -15,
+    seed + -15,
+    seed + -15,
+    seed + -25,
+    seed + -25,
+    seed + -25,
+  ];
+};
+
+const recursive = (animation, seed = 0, times = 15) => {
+  if (times === 0) return [];
+
+  const values = animation(seed);
+  return [
+    ...values,
+    ...recursive(animation, values[values.length - 1], times - 1),
+  ];
+};
+
+const zeros = (seed = 0) => {
+  return Array(6).fill(seed);
+};
+
+const sneak = {
+  start: {
+    opacity: [...recursive(zeros, 1, 6), 0],
+    rotate: [...recursive(zeros, 0, 4), 10, 0, 0],
+    scaleX: [
+      ...recursive(zeros, 1, 2),
+      ...recursive(jumpWidth, 0, 2),
+      ...recursive(zeros, 1, 3),
+    ],
+    scaleY: [
+      ...recursive(zeros, 1, 2),
+      ...recursive(jumpHeight, 0, 2),
+      ...recursive(zeros, 1, 3),
+    ],
+    y: [
+      ...recursive(zeros, 0, 2),
+      ...recursive(jump, 0, 2),
+      ...recursive(zeros, 0, 3),
+    ],
+    x: [
+      ...recursive(zeros, 0, 2),
+      ...recursive(goLeft, 0, 2),
+      ...recursive(zeros, -25 * 2, 2),
+      -25 * 2 - 10,
+    ],
+  },
+};
+
+const Sneak = ({ control, children, transition }) => {
+  return (
+    <Container>
+      <Word
+        animate={control}
+        variants={sneak}
+        transition={{ duration: 3, ...transition }}
+      >
+        {children.length ? children.charAt(0) : ""}
+      </Word>
+      <span>
+        {children.length ? children.substring(1, children.length) : ""}
+      </span>
+    </Container>
+  );
+};
+
+const Container = styled.span`
+  display: inline-flex;
+  overflow-x: hidden;
+  padding: 0;
+`;
+
+const Word = styled(motion.div)`
+  display: inline-block;
+  transform-origin: bottom right;
+  padding: 0;
+  padding-left: 50px;
+`;
+
+Sneak.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
+Sneak.displayName = "Sneak";
+
+export default Withmode(Sneak);
